7ec6e3eba948c44dec0b72f5537891b6c06d7a33
Spandan Mhapsekar <spandanmhapsekar@gmail.com>
2026-04-03 02:56:51 +0530

    Added tensor addition and substraction functions

--- include/tensor/tensor.hpp
+++ include/tensor/tensor.hpp
@@ -22,20 +22,27 @@ struct Tensor {
   TensorStorage *storage;
 };
 
+// Creation
 Tensor *tensor_create(Arena *arena, uint32_t ndims, const uint32_t *shape);
 Tensor *tensor_create_zeros(Arena *arena, uint32_t ndims,
                             const uint32_t *shape);
 
+// Views
 Tensor *tensor_view(Arena *arena, const Tensor *src);
 Tensor *tensor_reshape(Arena *arena, const Tensor *src, uint32_t ndims,
                        const uint32_t *shape);
 Tensor *tensor_transpose(Arena *arena, const Tensor *src, uint32_t dim0,
                          uint32_t dim1);
 
+// Utils
 uint64_t tensor_get_flat_index(const Tensor *t, const uint32_t *indices);
 void tensor_clear(Tensor *t);
 bool tensor_copy(Tensor *dst, const Tensor *src);
 void tensor_fill(Tensor *t, float val);
 bool tensor_is_contiguous(const Tensor *t);
+
+// Arithmetics
+bool tensor_add(Tensor *out, const Tensor *a, const Tensor *b);
+bool tensor_sub(Tensor *out, const Tensor *a, const Tensor *b);
 
 } // namespace gradientcore
---
+++ src/tensor/arithmetic/tensor_sub.cpp
@@ -0,0 +1,46 @@
+#include "../../../include/tensor/tensor.hpp"
+#include <cstdint>
+
+namespace gradientcore {
+
+bool tensor_sub(Tensor *out, const Tensor *a, Tensor *b) {
+  if (out == nullptr || a == nullptr || b == nullptr)
+    return false;
+  if (a->ndims != b->ndims || out->ndims != a->ndims)
+    return false;
+
+  for (uint32_t i = 0; i < a->ndims; i++) {
+    if (a->shape[i] != b->shape[i] || out->shape[i] != a->shape[i])
+      return false;
+  }
+
+  if (tensor_is_contiguous(a) && tensor_is_contiguous(b) &&
+      tensor_is_contiguous(out)) {
+    for (uint64_t i = 0; i < out->size; i++) {
+      out->storage->data[out->offset + i] =
+          a->storage->data[a->offset + i] - b->storage->data[b->offset + i];
+    }
+  } else {
+    uint32_t indices[MAX_TENSOR_DIMS] = {0};
+
+    for (uint64_t i = 0; i < out->size; i++) {
+      uint64_t flat_idx_out = tensor_get_flat_index(out, indices);
+      uint64_t flat_idx_a = tensor_get_flat_index(a, indices);
+      uint64_t flat_idx_b = tensor_get_flat_index(b, indices);
+
+      out->storage->data[flat_idx_out] =
+          a->storage->data[flat_idx_a] - b->storage->data[flat_idx_b];
+
+      for (int32_t d = out->ndims - 1; d >= 0; d--) {
+        indices[d]--;
+        if (indices[d] < out->shape[d])
+          break;
+        indices[d] = 0;
+      }
+    }
+  }
+
+  return true;
+}
+
+} // namespace gradientcore
